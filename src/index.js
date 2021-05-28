240b666b4061f4e336ff5253ce758c22f549c221
Sine Token <dev@solainetwork.com>
2021-05-28 18:39:35 +0530

    create a copy of mainpage

--- src/index.js
+++ src/index.js
@@ -6,10 +6,10 @@ import { Router, Route, Switch } from "react-router-dom";
 import "assets/scss/material-kit-react.scss?v=1.10.0";
 
 // pages for this product
-import Components from "views/Components/Components.js";
 import LandingPage from "views/LandingPage/LandingPage.js";
 import ProfilePage from "views/ProfilePage/ProfilePage.js";
 import LoginPage from "views/LoginPage/LoginPage.js";
+import MainPage from "views/MainPage/MainPage.js";
 
 var hist = createBrowserHistory();
 
@@ -19,7 +19,7 @@ ReactDOM.render(
       <Route path="/landing-page" component={LandingPage} />
       <Route path="/profile-page" component={ProfilePage} />
       <Route path="/login-page" component={LoginPage} />
-      <Route path="/" component={Components} />
+      <Route path="/" component={MainPage} />
     </Switch>
   </Router>,
   document.getElementById("root")
