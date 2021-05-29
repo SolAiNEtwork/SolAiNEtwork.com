ed3a55f516cdd12dd3ed28678037d01b3ffe6501
Sine Token <dev@solainetwork.com>
2021-05-29 19:46:41 +0530

    feat: Redesign the landing page

--- src/views/LandingPage/Sections/ProductSection.js
+++ src/views/LandingPage/Sections/ProductSection.js
@@ -21,13 +21,12 @@ export default function ProductSection() {
     <div className={classes.section}>
       <GridContainer justify="center">
         <GridItem xs={12} sm={12} md={8}>
-          <h2 className={classes.title}>Let{"'"}s talk product</h2>
+          <h2 className={classes.title}>Our Mission</h2>
           <h5 className={classes.description}>
-            This is the paragraph where you can write more details about your
-            product. Keep you user engaged by providing meaningful information.
-            Remember that by this time, the user is curious, otherwise he wouldn
-            {"'"}t scroll to get here. Add a button if you want the user to see
-            more.
+            Our mission is to create a ZERO commission platform for
+            crowd-sourcing. You can can get their work done fast through AI or
+            in a distributed manner through crowd-sourcing. You can also do the
+            work as per your skills and earn SINE.
           </h5>
         </GridItem>
       </GridContainer>
